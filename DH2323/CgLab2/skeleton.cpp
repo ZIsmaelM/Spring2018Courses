ae6f99bbbb11085ddfdd51cd6d805b61ab090b5f
ZIsmaelM <ismaelmarquez@utexas.edu>
2018-04-16 05:00:40 -0500

    Updated intersect function to Cramer intersect function

--- DH2323/CgLab2/skeleton.cpp
+++ DH2323/CgLab2/skeleton.cpp
@@ -16,6 +16,8 @@ const int SCREEN_WIDTH = 500;
 const int SCREEN_HEIGHT = 500;
 SDL_Surface* screen;
 int t;
+int testx = 0;
+int testy = 0;
 vector<Triangle> triangles;
 float focalLength = 1.0;//(SCREEN_WIDTH/2) / tan(45/2);
 vec3 cameraPos(0,0,-focalLength);
@@ -24,8 +26,13 @@ float maxFloat = std::numeric_limits<float>::max();
 // ----------------------------------------------------------------------------
 // FUNCTIONS
 
+struct Ray
+{
+	vec3 origin;
+	vec3 direction;
+};
 // Object that stores intersection data
-struct IntersectInfo
+struct Intersection
 {
 	vec3 position;
 	float distance = maxFloat;
@@ -35,9 +42,10 @@ struct IntersectInfo
 void Update();
 void Draw();
 void TriangleIntersect();
+void PrintVec3(vec3 v);
 bool IsEqual(vec3 A, vec3 B);
-vec3 IntersectPoint(vec3 rayOrig, vec3 rayDir, float t);
-bool ClosestIntersection(vec3 start, vec3 dir, const vector<Triangle>& triangles, IntersectInfo& ClosestIntersection);
+vec3 ComputeIntersectPoint(Ray, float t);
+bool ClosestIntersection(Ray, const vector<Triangle>& triangles, Intersection& ClosestIntersection);
 
 int main( int argc, char* argv[] )
 {
@@ -49,7 +57,7 @@ int main( int argc, char* argv[] )
 		Update();
 		Draw();
 		cout << "render finished" << endl;
-		usleep(25000000);	// delay for 25s
+		usleep(500000000);	// delay for 500s
 	}
 
 	SDL_SaveBMP( screen, "screenshot.bmp" );
@@ -74,14 +82,16 @@ void Draw()
 	LoadTestModel(triangles);
 	for( int y=0; y<SCREEN_HEIGHT; ++y ) {
 		for( int x=0; x<SCREEN_WIDTH; ++x ) {
-
+			testx = x;
+			testy = y;
 			// set ray direction and origin
-			vec3 rayOrig = cameraPos;
-			vec3 rayDir = vec3(x-(SCREEN_WIDTH/2), y-(SCREEN_HEIGHT/2), focalLength);
-			IntersectInfo intersect;
+			Ray ray;
+			ray.origin = cameraPos;
+			ray.direction = vec3(x-(SCREEN_WIDTH/2), y-(SCREEN_HEIGHT/2), focalLength);
+			Intersection intersect;
 
 			// check if an intersection was found
-			bool foo = ClosestIntersection(rayOrig, rayDir, triangles, intersect);
+			bool foo = ClosestIntersection(ray, triangles, intersect);
 			vec3 color(0,0,0);
 			if (foo) {
 				// cout << "rayOrig: (" << rayOrig.x << "," << rayOrig.y << "," << rayOrig.z << ")" << endl;
@@ -102,10 +112,7 @@ void Draw()
 	SDL_UpdateRect( screen, 0, 0, 0, 0 );
 }
 
-IntersectInfo TriangleIntersect(const vector<Triangle>& triangles, 
-	int index, 
-	vec3 rayOrig, 
-	vec3 rayDir) {
+Intersection TriangleIntersect(const vector<Triangle>& triangles, int index, Ray ray) {
 
 	// points on a ray: r = s + td
 	// s = ray orig
@@ -118,7 +125,7 @@ IntersectInfo TriangleIntersect(const vector<Triangle>& triangles,
 	// u & v = scalar values between 0 and 1, and u + v < 1
 
 	Triangle t = triangles[index];
-	IntersectInfo rayTri;
+	Intersection rayTri;
 
 	// calculate vectors parallel to triangle edges
 	vec3 e1 = t.v1 - t.v0;
@@ -126,51 +133,114 @@ IntersectInfo TriangleIntersect(const vector<Triangle>& triangles,
 
 	// s + td = v0 + u*e1 + v*e2
 	// s - v0 = u*e1 + v*e2 - td
-	vec3 b = rayOrig - t.v0;
+	vec3 b = ray.origin - t.v0;
 	//cout << b[0] << "	" << b[1] << "	" << b[2] << endl;
-	mat3 A( -rayDir, e1, e2);
+	mat3 A( -ray.direction, e1, e2);
 	vec3 x = glm::inverse(A) * b; // (t u v)
 
 	// do not update intersection if ray does not intersect triangle
-	if (x[0] < 0 || x[1] < 0 || x[2] < 0 || x[1] + x[2] > 1)
+	if (x[0] <= 0 || x[1] <= 0 || x[2] <= 0 || x[1] + x[2] >= 1)
 		return rayTri;
 
-	rayTri.position = IntersectPoint(rayDir, rayOrig, x[0]);
+	rayTri.position = ComputeIntersectPoint(ray, x[0]);
 	rayTri.distance = abs(x[0]);
 	rayTri.triangleIndex = index;
 	return rayTri;
 
 }
 
-bool ClosestIntersection(
-	vec3 start,
-	vec3 dir,
-	const vector<Triangle>& triangles,
-	IntersectInfo& ClosestIntersection) {
+Intersection CramerTriIntersection(const vector<Triangle>& triangles, int index, Ray ray) {
+    //Programming TASK 1: Implement this method
+    //Your code should compute the intersection between a ray and a triangle.
+    //
+    //If you detect an intersection, the return type should look similar to this:
+    //if(rayIntersectsTriangle)
+    //{
+    //  intersection = RayIntersection(ray,shared_from_this(),lambda,ray.normal,uvw);
+    //  return true;
+    //} 
+    //
+    // Hints :lambda
+    // Ray origin p_r : ray.getOrigin()
+    // Ray direction t_r : ray.getDirection()
+    // Compute the intersection point using ray.pointOnRay(lambda) 
+
+	Triangle tri = triangles[index];
+	Intersection noIntersect;
+
+	vec3 t1 = tri.v1 - tri.v0;
+	vec3 t2 = tri.v2 - tri.v0;
+	vec3 normal = glm::cross(t1, t2);
+
+	double d = glm::dot(ray.direction, normal);
+	//if (fabs(d) < 0.0001) return noIntersect;
+
+	double a = glm::dot((tri.v0 - ray.origin), normal);
+	double lambda = a / d;
+	//if (lambda < 0 || lambda + 0.0001 > 500) return noIntersect;
+
+	vec3 intersectPoint = ComputeIntersectPoint(ray, lambda);
+
+	// inside outside test
+	vec3 edge0 = tri.v1 - tri.v0;
+	vec3 edge1 = tri.v2 - tri.v1;
+	vec3 edge2 = tri.v0 - tri.v2;
+	vec3 intersectionV0 = intersectPoint - tri.v0;
+	vec3 intersectionV1 = intersectPoint - tri.v1;
+	vec3 intersectionV2 = intersectPoint - tri.v2;
+
+	vec3 C = glm::cross(edge0, intersectionV0);
+	if (glm::dot(normal, C) < 0) return noIntersect;
+	
+	C = glm::cross(edge1, intersectionV1);
+	if (glm::dot(normal, C) < 0) return noIntersect;
+
+	C = glm::cross(edge2, intersectionV2);
+	if (glm::dot(normal, C) < 0) return noIntersect;
+
+	Intersection triIntersect;
+	triIntersect.position = intersectPoint;
+	triIntersect.distance = abs(lambda);
+	triIntersect.triangleIndex = index;
+	return triIntersect;
+}
+
+bool ClosestIntersection(Ray ray, const vector<Triangle>& triangles, Intersection& ClosestIntersection) {
 
 	// set default intersect data
 	ClosestIntersection.triangleIndex = -1;
 
 	bool foo = false;
 
+	Intersection arr[triangles.size()];
 	// check if ray intersects any of the triangles
 	for (int i = 0; i < triangles.size(); i++) {
-		IntersectInfo newIntersection = TriangleIntersect(triangles, i, start, dir);
-
+		//Intersection newIntersection = TriangleIntersect(triangles, i, ray);
+		Intersection newIntersection = CramerTriIntersection(triangles, i, ray);
 		//cout << triangles[i].color << endl;
 		//cout << triangles[i].color[0] << "	" << triangles[i].color[1] << "	" << triangles[i].color[2] << endl;
 
 		// set new closest intersection if this triangle is closer than the previous closest
 		if (newIntersection.distance < ClosestIntersection.distance) {
-			vec3 gray = vec3(0.75,0.7385,0.75);
-			bool test = IsEqual(triangles[i].color, gray);
+			arr[i] = newIntersection;
+			vec3 blue = vec3(0.15,0.15,0.75);
+			bool test = IsEqual(triangles[i].color, blue);
+			if (test)
+				cout << "blue" << endl;
 			ClosestIntersection = newIntersection;
 			foo = true;
 		}
 	}
 
 	if (foo) {
-		//cout << "Intersect" << endl;	
+		
+		// if (testx > 200 && testx < 400 && testy > 200 && testy < 400) {
+		// 	for (int i = 0; i < triangles.size(); i++) {
+		// 		cout << "index " << i << ": " << arr[i].distance << endl;
+		// 	}
+		// 	cout << "Best :" << ClosestIntersection.distance << endl;
+		// 	PrintVec3(triangles[ClosestIntersection.triangleIndex].color);
+		// }	
 		return true;
 
 	}
@@ -185,11 +255,15 @@ bool IsEqual(vec3 A, vec3 B) {
 	return false;
 }
 
-vec3 IntersectPoint(vec3 rayOrig, vec3 rayDir, float t) {
+vec3 ComputeIntersectPoint(Ray ray, float t) {
 	vec3 point(0,0,0);
-	point.x = rayOrig.x + (t * rayDir.x);
-	point.y = rayOrig.y + (t * rayDir.y);
-	point.z = rayOrig.z + (t * rayDir.z);
+	point.x = ray.origin.x + (t * ray.direction.x);
+	point.y = ray.origin.y + (t * ray.direction.y);
+	point.z = ray.origin.z + (t * ray.direction.z);
 
 	return point;
+}
+
+void PrintVec3(vec3 v) {
+	cout << "(" << v[0] << ", " << v[1] << ", " << v[2] << ")" << endl;
 }
