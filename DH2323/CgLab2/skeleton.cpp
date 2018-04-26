f61a1541aeca3d6bc4f44126ea09ced807682f1d
ZIsmaelM <ismaelmarquez@utexas.edu>
2018-04-26 09:04:04 -0500

    DirectLight: correct light intensity, but incorrect colors.

--- DH2323/CgLab2/skeleton.cpp
+++ DH2323/CgLab2/skeleton.cpp
@@ -13,8 +13,8 @@ using glm::mat3;
 // ----------------------------------------------------------------------------
 // GLOBAL VARIABLES
 
-const int SCREEN_WIDTH = 100;
-const int SCREEN_HEIGHT = 100;
+const int SCREEN_WIDTH = 500;
+const int SCREEN_HEIGHT = 500;
 SDL_Surface* screen;
 int t;
 
@@ -27,13 +27,21 @@ float yaw = 0;
 vec3 r1 = vec3(cos(yaw),0,sin(yaw));
 vec3 r2 = vec3(0,1,0);
 vec3 r3 = vec3(-sin(yaw),0,cos(yaw));
-//mat3 R = (r1,r2,r3);
+mat3 R = mat3(r1,r2,r3);
 
+//camera
 float camX = 0;
 float camY = 0;
 float camZ = -3;
+vec3 camOrig = vec3(camX,camY,camZ);
 vec3 cameraPos = vec3(camX,camY,camZ);	// -1 to 1 in x,y,z coords
+vec3 camRight;
+vec3 camDown;
+vec3 camForward;
 
+//light
+vec3 lightPos(0,-0.5,-0.7);
+vec3 lightColor = 14.f * vec3(1,1,1);
 // ----------------------------------------------------------------------------
 // STRUCTS
 
@@ -54,6 +62,8 @@ struct Intersection {
 void Update();
 void Draw();
 bool ClosestIntersection(Ray ray, const vector<Triangle>& triangles, Intersection& closestIntersection);
+vec3 DirectLight(const Intersection& i);
+float CalculateDistance(vec3 a, vec3 b);
 
 float ComputeDistance(vec3 v, vec3 u) {
 	float a = pow(u[0]-v[0],2);
@@ -63,11 +73,12 @@ float ComputeDistance(vec3 v, vec3 u) {
 	return float(sqrt(a + b + c));
 }
 
-float Rotation() {
-	r1 = vec3(cos(yaw),0,sin(yaw));
+void Rotate() {
+	int temp = 2;
+	r1 = vec3(temp*cos(yaw),0,temp*sin(yaw));
 	r2 = vec3(0,1,0);
-	r3 = vec3(-sin(yaw),0,cos(yaw));
-	//R = (r1,r2,r3);
+	r3 = vec3(temp*(-sin(yaw)),0,temp*cos(yaw));
+	R = mat3(r1,r2,r3);
 }
 
 int main( int argc, char* argv[] )
@@ -96,29 +107,41 @@ void Update()
 	t = t2;
 	cout << "Render time: " << dt << " ms." << endl;
 
-	Uint8* keystate = SDL_GetKeyState( 0 );
-	if( keystate[SDLK_UP] ) {
-		// Move camera forward
-		camZ += speed;
-		cameraPos = vec3(camX,camY,camZ);
-	}
-	if( keystate[SDLK_DOWN] ) {
-		// Move camera backward
-		camZ -= speed;
-		cameraPos = vec3(camX,camY,camZ);
-	}
-	if( keystate[SDLK_LEFT] ) {
-		// Move camera to the left
-		camX -= speed;
-		yaw -= speed;
+	// Uint8* keystate = SDL_GetKeyState( 0 );
+	// if( keystate[SDLK_UP] ) {
+	// 	// Move camera forward
+	// 	camZ += speed;
+	// 	cameraPos = vec3(camX,camY,camZ);
+	// }
+	// if( keystate[SDLK_DOWN] ) {
+	// 	// Move camera backward
+	// 	camZ -= speed;
+	// 	cameraPos = vec3(camX,camY,camZ);
+	// }
+	// if( keystate[SDLK_LEFT] ) {
+	// 	// Move camera to the left
+	// 	camX -= speed;
+	// 	yaw -= M_PI/16;
 
-		cameraPos = vec3(camX,camY,camZ);
-	}
-	if( keystate[SDLK_RIGHT] ) {
-		// Move camera to the right
-		camX += speed;
-		cameraPos = vec3(camX,camY,camZ);
-	}
+	// 	Rotate();
+	// 	camRight = vec3(R[0][0], R[0][1], R[0][2]);
+	// 	camDown = vec3(R[1][0], R[1][1], R[1][2]);
+	// 	camForward = vec3(R[2][0], R[2][1], R[2][2]);
+
+	// 	cameraPos = R[0];//camRight + camForward + camOrig; //vec3(camX,camY,camZ) + right + forward;
+	// }
+	// if( keystate[SDLK_RIGHT] ) {
+	// 	// Move camera to the right
+	// 	camX += speed;
+	// 	yaw += M_PI/16;
+
+	// 	Rotate();
+	// 	camRight = vec3(R[0][0], R[0][1], R[0][2]);
+	// 	camDown = vec3(R[1][0], R[1][1], R[1][2]);
+	// 	camForward = vec3(R[2][0], R[2][1], R[2][2]);
+
+	// 	cameraPos = R[0];//camRight + camForward + camOrig; //vec3(camX,camY,camZ) + right + forward;
+	// }
 }
 
 void Draw()
@@ -135,12 +158,13 @@ void Draw()
 		for( int x=0; x<SCREEN_WIDTH; ++x )
 		{
 			ray.orig = cameraPos;
-			ray.dir = vec3(x-SCREEN_WIDTH/2, (y-SCREEN_HEIGHT/2), focalLength);
+			ray.dir = camRight + camForward + vec3(x-SCREEN_WIDTH/2, (y-SCREEN_HEIGHT/2), focalLength);
 
 			vec3 color = vec3(0,0,0);
 			if(ClosestIntersection(ray, triangles, pixel)) {
 				Triangle tri = triangles[pixel.index];
-				color = tri.color;
+				vec3 light = DirectLight(pixel);
+				color = light;//tri.color;
 			}
 
 			PutPixelSDL( screen, x, y, color );
@@ -184,4 +208,45 @@ bool ClosestIntersection(Ray ray, const vector<Triangle>& triangles, Intersectio
 	}
 
 	return intersectDetected;
+}
+
+vec3 DirectLight(const Intersection& i) {
+
+	Triangle t = triangles[i.index];
+	float r = CalculateDistance(lightPos,i.pos);
+	float A = 4*M_PI*pow(r,2);
+
+	vec3 B = lightColor / A;
+	// B[0] = lightColor[0] / A;
+	// B[1] = lightColor[1] / A;
+	// B[2] = lightColor[2] / A;
+
+	vec3 D = vec3(0,0,0);
+	D[0] = B[0]*std::max(t.normal[0],0.0f);
+	D[1] = B[1]*std::max(t.normal[1],0.0f);
+	D[2] = B[2]*std::max(t.normal[2],0.0f);
+
+	return D;
+}
+
+void InterpolateVec3( vec3 a, vec3 b, vector<vec3>& result) {
+	int numSamples = result.size();
+	int numPoints = 3;
+
+	int dist;
+	for (int i = 0; i < numSamples; ++i) {
+		for (int j = 0; j < numPoints; ++j) {
+			float dist = b[j] - a[j];
+			float t = ((float)i/(numSamples-1)) * dist;
+			result[i][j] = a[j] + t;
+		}
+	}
+}
+
+float CalculateDistance(vec3 a, vec3 b) {
+	float x = b[0] - a[0];
+	float y = b[1] - a[1];
+	float z = b[2] - a[2];
+
+	return sqrt(pow(x,2)+pow(y,2)+pow(z,2));
 }
